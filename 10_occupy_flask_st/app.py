3444f689c1630709158221871f0ec15d81d48674
Jack Lu <jlu9@stuy.edu>
2018-09-21 14:01:32 -0400

    started hw 10

---
+++ 10_occupy_flask_st/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    link = '<a href="./occupations">go here</a>'
+    return link
+
+@app.route('/occupations')
+def occupy():
+    return render_template(
+            'template.html',
+            foo='./data/occupations.csv')
