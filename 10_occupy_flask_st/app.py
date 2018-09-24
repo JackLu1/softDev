e884daab34c47d76dfe818c2aa06838f6e2a8b14
jlu <jlu2025@gmail.com>
2018-09-23 23:46:26 -0400

    Completed K10

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -1,14 +1,40 @@
+# ZhouLu - Jack Lu, Wei Wen Zhou
+# SoftDev pd8
+# K10 -- Jinja Tuning
+# 2018-09-22
+
 from flask import Flask, render_template
+import csv
+import random
 
 app = Flask(__name__)
 
+    # Builds a dictionary of careers occupations and percentage of US workforce.
+dict = {}
+with open('./data/occupations.csv', 'rt') as csvfile:
+    reader = csv.reader(csvfile)
+    first = reader.__next__() # Processes column labels Job Class, Percentage
+    dict[first[0]] = first[1]
+    for i in reader:
+        dict[ i[0] ] = float( i[1])
+
+# Choose random job based on percentage
+jobList = list(dict.keys())
+chanceList = list(dict.values())
+
 @app.route('/')
 def index():
-    link = '<a href="./occupations">go here</a>'
-    return link
+    return '<a href="./occupations">click this</a>'
 
 @app.route('/occupations')
-def occupy():
+def render():
+    randJob = random.choices(jobList[1:-1], chanceList[1:-1])[0]
     return render_template(
             'template.html',
-            foo='./data/occupations.csv')
+            title = 'Title',
+            rand = randJob,
+            occupations = dict
+            )
+
+app.debug = True
+app.run()
