183217cff87482d1c131d5bf6cb636c508f2d749
Jack Lu <jlu9@stuy.edu>
2018-09-25 14:01:23 -0400

    refactored K10

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -4,29 +4,9 @@
 # 2018-09-22
 
 from flask import Flask, render_template
-import csv
-import random
+import util.process as process
 
 app = Flask(__name__)
-
-# Builds dictionary of occupations and corresponding percentage of workforce.
-def buildDict(filename):
-    d = {}
-    with open(filename, 'rt') as csvfile:
-        reader = csv.reader(csvfile)
-        first = reader.__next__() # Processes column labels Job Class, Percentage
-        d[first[0]] = first[1]
-        for i in reader:
-            d[ i[0] ] = float( i[1])
-    return d
-
-# Choose random job based on percentage
-def chooseRandom(dictionary):
-    jobList = list(dictionary.keys()) # list of jobs
-    chanceList = list(dictionary.values()) # list of percentages
-    # Ignores first and last rows (heading and total)
-    randJob = random.choices(jobList[1:-1], chanceList[1:-1])[0]
-    return randJob
 
 # Root directory with link to table of occupations.
 @app.route('/')
@@ -37,11 +17,11 @@ def index():
 # Calls chooseRandom to choose Occupation based on relative percentage
 @app.route('/occupations')
 def render():
-    dict = buildDict('./data/occupations.csv')
+    dict = process.buildDict('./data/occupations.csv')
     return render_template(
             'template.html',
             title = 'Title',
-            rand = chooseRandom(dict),
+            rand = process.chooseRandom(dict),
             occupations = dict
             )
 
