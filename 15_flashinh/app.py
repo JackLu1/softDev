7cbcacd1e93671e58eb5b02bc2c8a934af191fd2
jack <jlu2025@gmail.com>
2018-10-02 23:50:52 -0400

    flashing working

--- 15_flashinh/app.py
+++ 15_flashinh/app.py
@@ -3,7 +3,7 @@
 # K14 -- Do I Know You?
 # 2018-10-1
 
-from flask import Flask, render_template, request, session, url_for, redirect
+from flask import Flask, render_template, request, session, url_for, redirect, flash
 import os
 
 app = Flask(__name__)
@@ -26,12 +26,12 @@ def authenticate():
     check_pw = request.form["pass"]
 
     if check_pw != pw and check_usr != usr:
-        return render_template('error.html',
-            title='Error',
-            error_message='Incorrect login information')
+        flash('Unsuccessful login, try again')
+        return redirect(url_for('index'))
 
     # add username to session
     session[usr] = pw
+    flash('Successfully logged in')
     return redirect(url_for('index'))
 
 @app.route('/logout', methods=["POST"])
