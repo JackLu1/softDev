8aa6cf63b036ba5b6cce5f538d82ad1672568348
Jack Lu <jlu2025@gmail.com>
2018-12-20 01:40:42 -0500

    d

---
+++ 29_js/funky_town.js
@@ -0,0 +1,62 @@
+// team JL - Jack Lu, Joyce Liao
+// SoftDev1 pd8
+// K29 -- Sequential Progression
+// 2018-12-20
+
+var fib = function (n){
+    if (n < 2){
+        return n;
+    }
+    else
+        return fib(n-1) + fib(n-2);
+}
+
+var gcd = function(a ,b) {
+    if ( a < b )
+        var c = a;
+    else
+        var c = b;
+    for (var i = c; i >= 1; i--) {
+        if (a % i == 0 && b % i == 0)
+            return i;
+    }
+    return 1;
+}
+
+var students = [
+    "J", "A", "D", "E"
+];
+
+var randomStudent = function(){
+    var i = Math.floor(Math.random() * students.length);
+
+    return students[i];
+}
+
+var fibbut = document.getElementById("fib");
+var gcdbut = document.getElementById("gcd");
+var ranbut = document.getElementById("random");
+// console.log(ranbut);
+var d_block = document.getElementById("data");
+
+var get_fib_7 = function() {
+    data = fib(7);
+    console.log(data);
+    d_block.innerHTML = "7th fib is " + data;
+}
+
+var get_gcd_2490 = function() {
+    data = gcd(24,90);
+    console.log(data);
+    d_block.innerHTML = "GCD of 24 and 90 is " + data;
+}
+
+var get_ran = function() {
+    data = randomStudent();
+    console.log(data);
+    d_block.innerHTML = "random student: " + data;
+}
+
+fibbut.addEventListener('click', get_fib_7);
+gcdbut.addEventListener('click', get_gcd_2490);
+ranbut.addEventListener('click', get_ran);
